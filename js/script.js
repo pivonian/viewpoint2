84a0aa79404aa938684e64643a351ff22725c1c9
KettleLord <bodhireimer@gmail.com>
2025-12-05 18:05:04 +1000

    add show info & make the navbar go ssolid sooner

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener("scroll", () => {
     const nav = document.querySelector(".nav");
     
-    if (window.scrollY > 800) {   // scroll distance before it becomes solid
+    if (window.scrollY > 200) {   // scroll distance before it becomes solid
         nav.classList.add("scrolled");
     } else {
         nav.classList.remove("scrolled");
